c8944f8a6202c57d8725e63d5557e980f00bf736
Shuhei Ohno <ohno.github@gmail.com>
2022-11-14 19:19:41 +0900

    Update data

--- new/data.f90
+++ new/data.f90
@@ -3,12 +3,14 @@ subroutine data()
 integer :: A = 1
 integer :: B(3) = [1,2,3]
 integer :: C(3) = [(3*i, i=1,3)]
-integer :: D(2,3) = reshape([1,2,3,4,5,6], [2,3])
+integer :: D(3) = [(4, i=1,3)]
+integer :: E(2,3) = reshape([1,2,3,4,5,6], [2,3])
 
 print *, A
 print *, B
 print *, C
-print *, D(1,:)
-print *, D(2,:)
+print *, D
+print *, E(1,:)
+print *, E(2,:)
 
 end subroutine
--- old/data.f
+++ old/data.f
@@ -3,17 +3,20 @@
       
       DIMENSION B(3)
       DIMENSION C(3)
-      DIMENSION D(2,3)
+      DIMENSION D(3)
+      DIMENSION E(2,3)
       
       DATA A/1/
       DATA B/1,2,3/
       DATA C/3,6,9/
-      DATA D/1,2,3,4,5,6/
+      DATA D/3*4/
+      DATA E/1,2,3,4,5,6/
       
       PRINT *, A
       PRINT *, B
       PRINT *, C
-      PRINT *, D(1,:)
-      PRINT *, D(2,:)
+      PRINT *, D
+      PRINT *, E(1,:)
+      PRINT *, E(2,:)
       
       END SUBROUTINE
