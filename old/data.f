2f2fa8f811c3ea6dd940ca9ca8f6d25c0ee0df85
Shuhei Ohno <ohno.github@gmail.com>
2022-11-14 19:05:41 +0900

    Update data

--- old/data.f
+++ old/data.f
@@ -1,19 +1,19 @@
-			SUBROUTINE DATA()
-			IMPLICIT INTEGER (A-Z)
-
-			DIMENSION B(3)
-			DIMENSION C(3)
-			DIMENSION D(2,3)
-
-			DATA A/1/
-			DATA B/1,2,3/
-			DATA C/3,6,9/
-			DATA D/1,2,3,4,5,6/
-
-			PRINT *, A
-			PRINT *, B
-			PRINT *, C
-	    PRINT *, D(1,:)
-	    PRINT *, D(2,:)
-
-			END SUBROUTINE
+      SUBROUTINE DATA()
+      IMPLICIT INTEGER (A-Z)
+      
+      DIMENSION B(3)
+      DIMENSION C(3)
+      DIMENSION D(2,3)
+      
+      DATA A/1/
+      DATA B/1,2,3/
+      DATA C/3,6,9/
+      DATA D/1,2,3,4,5,6/
+      
+      PRINT *, A
+      PRINT *, B
+      PRINT *, C
+      PRINT *, D(1,:)
+      PRINT *, D(2,:)
+      
+      END SUBROUTINE
