7ac00a95ceb849766d5cb15bc51f0fd7d063d52f
Shuhei Ohno <ohno.github@gmail.com>
2022-11-14 20:29:29 +0900

    Update data

--- new/data.f90
+++ new/data.f90
@@ -4,13 +4,18 @@ integer :: A = 1
 integer :: B(3) = [1,2,3]
 integer :: C(3) = [(3*i, i=1,3)]
 integer :: D(3) = [(4, i=1,3)]
-integer :: E(2,3) = reshape([1,2,3,4,5,6], [2,3])
+integer :: E(5) = [(4, i=1,3), 5, 6]
+integer :: F(3) = [1,2,3], G(3) = [4,5,6]
+integer :: H(2,3) = reshape([1,2,3,4,5,6], [2,3])
 
-print *, A
-print *, B
-print *, C
-print *, D
-print *, E(1,:)
-print *, E(2,:)
+print *, "A = ", A
+print *, "B = ", B
+print *, "C = ", C
+print *, "D = ", D
+print *, "E = ", E
+print *, "F = ", F
+print *, "G = ", G
+print *, "H(1,:) = ", H(1,:)
+print *, "H(2,:) = ", H(2,:)
 
 end subroutine
--- old/data.f
+++ old/data.f
@@ -4,19 +4,27 @@
       DIMENSION B(3)
       DIMENSION C(3)
       DIMENSION D(3)
-      DIMENSION E(2,3)
+      DIMENSION E(5)
+      DIMENSION F(3)
+      DIMENSION G(3)
+      DIMENSION H(2,3)
       
       DATA A/1/
       DATA B/1,2,3/
       DATA C/3,6,9/
       DATA D/3*4/
-      DATA E/1,2,3,4,5,6/
+      DATA E/3*4,5,6/
+      DATA F,G/1,2,3,4,5,6/
+      DATA H/1,2,3,4,5,6/
       
-      PRINT *, A
-      PRINT *, B
-      PRINT *, C
-      PRINT *, D
-      PRINT *, E(1,:)
-      PRINT *, E(2,:)
-      
+      PRINT *, "A = ", A
+      PRINT *, "B = ", B
+      PRINT *, "C = ", C
+      PRINT *, "D = ", D
+      PRINT *, "E = ", E
+      PRINT *, "F = ", F
+      PRINT *, "G = ", G
+      PRINT *, "H(1,:) = ", H(1,:)
+      PRINT *, "H(2,:) = ", H(2,:)
+
       END SUBROUTINE
